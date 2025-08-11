30370b422c925a99867d3437722725618a2ad102
deviliaan <61916927+deviliaan@users.noreply.github.com>
2025-08-11 18:02:18 +0530

    Add Flask and dependencies to project
    Installed Flask and its core dependencies (Werkzeug, Jinja2, Click, MarkupSafe, ItsDangerous, Blinker, Colorama, Importlib_metadata, Zipp) to enable web application development. Updated app.py and config.ini to support Flask integration.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import time
 from config import Config
-
+from flask import Flask
 # Config Parser Class
 # Initialize the Config object
 config = Config()
@@ -8,11 +8,18 @@ config = Config()
 # Last run time
 config.add('Settings', 'last_run', time.strftime('%Y-%m-%d %H:%M:%S'))
 
+# Server configuration
+config.add('Server','host','127.0.0.1')
+config.add('Server','port','5000')
+
+
+# Create Flask app
+app = Flask(__name__)
 
 
 
-
-
+# Load configuration from config.ini And Run the App
+app.run(debug=True,host=config.get('Server', 'host'), port=int(config.get('Server', 'port')))
 
 
 
